256f8d1a6ca6b40b2f8a937efcb1ea8c1358c0cb
hirotosakurai <k021g1130@g.neec.ac.jp>
2022-11-30 15:46:42 +0900

    授業内容

--- PG3_2-1/main.cpp
+++ PG3_2-1/main.cpp
@@ -1,63 +1,9 @@
 #include <stdio.h>
-#include <list>
-#include <string.h>
+#include <stdlib.h>
 
-using namespace std;
-
-int main()
+typedef struct cell
 {
-	list<const char*>stations =
-	{
-		"Shinagawa","Tamachi",
-		"Hamamatsucho",
-		"Shinbashi","Yurakucho",
-		"Tokyo","Kanda","Akihabara",
-		"Okachimachi","Ueno",
-		"Uguisudani","Nippori",
-		"Tabata"
-	};
+	char str[8];
+	struct cell* next;
 
-
-	//1970年
-	printf("---1970---\n");
-	for (auto itr = stations.begin(); itr != stations.end(); itr++)
-	{
-		printf("%s\n", *itr);
-	}
-
-	//1971年
-	for (auto itr = stations.begin(); itr != stations.end(); itr++)
-	{
-		if (strcmp(*itr, "Tabata") == 0)
-		{
-			stations.insert(itr, "Nishi-Nippori");
-			break;
-		}
-	}
-
-	//2019年
-	printf("---2019---\n");
-	for (auto itr = stations.begin(); itr != stations.end(); itr++)
-	{
-		printf("%s\n", *itr);
-	}
-
-	//2020年
-	for (auto itr = stations.begin(); itr != stations.end(); itr++)
-	{
-		if (strcmp(*itr, "Tamachi") == 0)
-		{
-			stations.insert(itr, "Takanawa-Geto-Way");
-			break;
-		}
-	}
-
-	//2022年
-	printf("---2022---\n");
-	for (auto itr = stations.begin(); itr != stations.end(); itr++)
-	{
-		printf("%s\n", *itr);
-	}
-
-	return 0;
-}
+} CELL;
