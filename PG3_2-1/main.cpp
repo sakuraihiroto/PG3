3beb337004ccdec3e04f8b7f574e1611707165a4
hirotosakurai <k021g1130@g.neec.ac.jp>
2022-12-02 08:31:09 +0900

    単方向リスト

--- PG3_2-1/main.cpp
+++ PG3_2-1/main.cpp
@@ -1,9 +1,69 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+
+using namespace std;
+
 
 typedef struct cell
 {
-	char str[8];
+	int val;
 	struct cell* next;
+}CELL;
 
-} CELL;
+void create(CELL* head, int val);	//データを追加する関数のプロトタイプ宣言　
+void index(CELL* head);	//一覧を表示する関数のプロトタイプ宣言
+
+int main()
+{
+	int val;
+
+	CELL head;
+	head.next = nullptr;
+
+	while (true)
+	{
+		printf("好きな数字を入力してください\n入力された値:");
+		scanf_s(" %d", &val);
+
+		//最後尾にセルを追加
+		create(&head, val);
+
+		printf("\n入力された値一覧: ");
+		//リスト一覧の表示
+		index(&head);
+
+		printf("\n\n");
+	}
+
+	return 0;
+}
+
+void create(CELL* head, int val)
+{
+	CELL* beta;
+	//新規作成するセル分のメモリを確保する
+	beta = (CELL*)malloc(sizeof(CELL));
+
+	beta->val = val;
+	beta->next = nullptr;
+
+	//最後(最新)のセルのアドレスの1つめの処理は引数から持ってきた
+	//リストのうち最初にセルのアドレスが該当する
+	while (head->next != nullptr) {
+		head = head->next;
+
+	}
+
+	head->next = beta;
+
+}
+
+void index(CELL* head) {
+	while (head->next != nullptr) {
+		head = head->next;
+		
+		printf("[%d]", head->val);
+		
+	}
+}
